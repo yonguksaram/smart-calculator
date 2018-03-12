c3f391366b4e8ec0219a1e80d4306100d7d265be
Stanislau Alimpau <alimberg@list.ru>
2018-03-12 22:02:55 +0300

    smart-calculator/feature: adding code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
-class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
-
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
-
-  multiply(number) {
-    // your implementation
-  }
-
-  devide(number) {
-    // your implementation
-  }
-
-  pow(number) {
-    // your implementation
-  }
+function calculateString(str) {
+  return (new Function('return ' + str))();
 }
+
+
+class SmartCalculator {
+    constructor(initialValue){
+        this.result = String(initialValue);
+
+    }
+
+    toString(){
+        return this.calculate();
+    }
+
+    add(number){
+      this.result = this.result+"+"+number;
+        return this
+    }
+
+    subtract(number){
+      this.result = this.result+"-"+number;
+        return this
+    }
+
+    multiply(number){
+      this.result = this.result+"*"+number;
+        return this
+    }
+
+    devide(number){
+      this.result = this.result+"/"+number;
+        return this
+    }
+
+    pow(number){
+      for(var i=this.result.length-2;i>0;i--) {
+        if(["+","-","*","/"].indexOf(this.result[i])>0){
+      this.result = this.result.substring(0,i+1)+"Math.pow("+this.result.substring(i+1)+","+number+")";
+      break
+        }
+      }
+        return this
+    }
+
+    calculate(){
+      return calculateString(this.result);
+    }
+  }
 
 module.exports = SmartCalculator;
